193e177afbf36d860674b8294c543dbc3f1c2a4e
yashmaheshjaiswal <117263174+yashmaheshjaiswal@users.noreply.github.com>
2023-01-17 13:50:15 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import "../styles/App.css";
+import { movies } from "../utils/movieList";
+
+const App = () => {
+    const [inputValue, setInputValue] = useState("");
+    const [results, setResult] = useState(null);
+
+    function handleClick(e) {}
+
+    function handleChange(e) {}
+
+    return (
+        <div id="main">
+            <form id="form">
+                <input
+                    type="text"
+                    onChange={handleChange}
+                    value={inputValue}
+                    id="movie-input"
+                ></input>
+                <button id="button" onClick={handleClick}>
+                    Search
+                </button>
+            </form>
+            <div id="result">
+                {results &&
+                    results.map((movie) => (
+                        <div className="movie">{movie}</div>
+                    ))}
+            </div>
+        </div>
+    );
+};
+
+export default App;
